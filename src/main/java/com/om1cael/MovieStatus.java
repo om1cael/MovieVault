e717688b7ff358af8e1ed104a1c6df74e0c00a9a
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-09 15:47:12 -0300

    refactor: add description to the movie statuses

--- src/main/java/com/om1cael/MovieStatus.java
+++ src/main/java/com/om1cael/MovieStatus.java
@@ -1,7 +1,17 @@
 package com.om1cael;
 
 public enum MovieStatus {
-    Watched,
-    ToWatch,
-    NoInterest
+    WATCHED("Watched"),
+    TO_WATCH("To Watch"),
+    NO_INTEREST("No Interest");
+
+    private final String description;
+
+    MovieStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
