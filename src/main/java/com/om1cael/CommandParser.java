c8d804c275436b17d63988f5e0c77700ecb3d368
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-09 15:39:36 -0300

    feat: add a basic command parser

---
+++ src/main/java/com/om1cael/CommandParser.java
@@ -0,0 +1,55 @@
+package com.om1cael;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public final class CommandParser {
+    HashMap<Integer, Actions> choices = new HashMap<>(Map.of(
+            1, Actions.ADD_MOVIE,
+            2, Actions.REMOVE_MOVIE
+    ));
+
+    public void getInitialInput() {
+        Scanner scanner = new Scanner(System.in);
+        showChoices();
+        handleInput(scanner);
+    }
+
+    private void handleInput(Scanner scanner) {
+        System.out.print("Your choice: ");
+        final int inputChoice = scanner.nextInt();
+
+        if(!choices.containsKey(inputChoice)) {
+            System.out.println("This option is not valid.");
+            return;
+        }
+
+        switch (choices.get(inputChoice)) {
+            case ADD_MOVIE -> System.out.println("You choose to add a movie");
+            case REMOVE_MOVIE -> System.out.println("You choose to remove a movie");
+            default -> System.out.println("Not a valid option.");
+        }
+    }
+
+    private void showChoices() {
+        choices.forEach((k, v) -> {
+            System.out.println("[" + k + "]: " + v.getDescription());
+        });
+    }
+}
+
+enum Actions {
+    ADD_MOVIE("Add a new movie"),
+    REMOVE_MOVIE("Remove a movie");
+
+    private final String description;
+
+    Actions(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- src/main/java/com/om1cael/Main.java
+++ src/main/java/com/om1cael/Main.java
@@ -2,6 +2,7 @@ package com.om1cael;
 
 public class Main {
     public static void main(String[] args) {
-
+        CommandParser commandParser = new CommandParser();
+        commandParser.getInitialInput();
     }
 }
