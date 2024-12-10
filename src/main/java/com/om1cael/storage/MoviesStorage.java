3ae73ccf9b96ecb6be7332a5d077bc8e7f3c6a4c
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-10 09:39:29 -0300

    feat: add gson

--- src/main/java/com/om1cael/storage/MoviesStorage.java
+++ src/main/java/com/om1cael/storage/MoviesStorage.java
@@ -1,11 +1,8 @@
 package com.om1cael.storage;
 
-import com.om1cael.enums.MovieStatus;
 import com.om1cael.models.Movie;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 // TODO: The content must be loaded from a JSON or text file.
 public class MoviesStorage {
