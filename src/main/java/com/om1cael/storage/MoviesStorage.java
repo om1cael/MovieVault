1aea1ad0dc7e366daf00a3a92db5626e9e301ca9
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-10 11:22:31 -0300

    refactor: remove completed "todo"

--- src/main/java/com/om1cael/storage/MoviesStorage.java
+++ src/main/java/com/om1cael/storage/MoviesStorage.java
@@ -2,10 +2,8 @@ package com.om1cael.storage;
 
 import com.om1cael.models.Movie;
 
-import java.util.ArrayList;
 import java.util.List;
 
-// TODO: The content must be loaded from a JSON or text file.
 public class MoviesStorage {
     StorageHandler storageHandler;
 
